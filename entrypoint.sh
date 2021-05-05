c100ba8596cb2dbcc90c5e8a0eda865d42da8a3d
treastrain / Tanaka Ryoga <tr@tret.jp>
2021-05-06 02:42:47 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,14 +35,14 @@ fi
 
 add_mask "${GH_PERSONAL_ACCESS_TOKEN}"
 
-if [ -z "${WIKI_COMMIT_MESSAGE:-}" ]; then
-    debug "WIKI_COMMIT_MESSAGE not set, using default"
-    WIKI_COMMIT_MESSAGE='Automatically publish wiki'
+if [ -z "${GH_PAGES_COMMIT_MESSAGE:-}" ]; then
+    debug "GH_PAGES_COMMIT_MESSAGE not set, using default"
+    GH_PAGES_COMMIT_MESSAGE='Automatically publish GitHub Pages'
 fi
 
-GIT_REPOSITORY_URL="https://${GH_PERSONAL_ACCESS_TOKEN}@${GITHUB_SERVER_URL#https://}/$GITHUB_REPOSITORY.wiki.git"
+GIT_REPOSITORY_URL="https://${GH_PERSONAL_ACCESS_TOKEN}@${GITHUB_SERVER_URL#https://}/$GITHUB_REPOSITORY.git"
 
-debug "Checking out wiki repository"
+debug "Checking out gh-pages repository"
 tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 (
     cd "$tmp_dir" || exit 1
@@ -50,19 +50,17 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
     git config user.name "$GITHUB_ACTOR"
     git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
     git pull "$GIT_REPOSITORY_URL"
+    git checkout gh-pages
 ) || exit 1
 
 debug "Enumerating contents of $1"
-for file in $(find $1 -maxdepth 1 -type f -name '*.md' -execdir basename '{}' ';'); do
-    debug "Copying $file"
-    cp "$1/$file" "$tmp_dir"
-done
+cp -r "$1" "$tmp_dir"
 
 debug "Committing and pushing changes"
 (
     cd "$tmp_dir" || exit 1
     git add .
-    git commit -m "$WIKI_COMMIT_MESSAGE"
+    git commit -m "$GH_PAGES_COMMIT_MESSAGE"
     git push --set-upstream "$GIT_REPOSITORY_URL" master
 ) || exit 1
 
