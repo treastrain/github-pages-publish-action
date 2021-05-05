2c4b02278b3b6f2a62d99ad5f3003b0984c944e3
treastrain / Tanaka Ryoga <tr@tret.jp>
2021-05-06 03:03:45 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,26 +42,26 @@ fi
 
 GIT_REPOSITORY_URL="https://${GH_PERSONAL_ACCESS_TOKEN}@${GITHUB_SERVER_URL#https://}/$GITHUB_REPOSITORY.git"
 
-debug "Checking out gh-pages repository"
-tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
-(
-    cd "$tmp_dir" || exit 1
-    git init
-    git config user.name "$GITHUB_ACTOR"
-    git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
-    git pull "$GIT_REPOSITORY_URL" gh-pages
-) || exit 1
+# debug "Checking out gh-pages repository"
+# tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+# (
+#     cd "$tmp_dir" || exit 1
+#     git init
+#     git config user.name "$GITHUB_ACTOR"
+#     git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+#     git pull "$GIT_REPOSITORY_URL" gh-pages
+# ) || exit 1
 
 debug "Enumerating contents of $1"
 cp -r "$1" "$tmp_dir"
 
-debug "Committing and pushing changes"
-(
-    cd "$tmp_dir" || exit 1
-    git add .
-    git commit -m "$GH_PAGES_COMMIT_MESSAGE"
-    git push --set-upstream "$GIT_REPOSITORY_URL" gh-pages
-) || exit 1
+# debug "Committing and pushing changes"
+# (
+#     cd "$tmp_dir" || exit 1
+#     git add .
+#     git commit -m "$GH_PAGES_COMMIT_MESSAGE"
+#     git push --set-upstream "$GIT_REPOSITORY_URL" gh-pages
+# ) || exit 1
 
-rm -rf "$tmp_dir"
+# rm -rf "$tmp_dir"
 exit 0
